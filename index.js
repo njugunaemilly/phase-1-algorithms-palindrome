6491db9c0b4b30f2820ce6e79a2ece8b4b3d4590
Emilly <emilly.wanjiku@student.moringaschool.com>
2022-12-12 22:07:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseStringWord(word);
+  return word === reversedWord;
+
+  function reverseStringWord(word){
+    return word.split("").reverse().join("");
+  }
 }
+  
 
 /* 
   Add your pseudocode here
+  make a isPalindrome fucntion
+  flip input string
+  if flipped string is the same as the input string
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  should return true if the input string is the same forward and backeard, words like kayak,rotator, racecar, radar
+  if the input string is not the same when flipped, function should return false
 */
 
 // You can run `node index.js` to view these console logs
